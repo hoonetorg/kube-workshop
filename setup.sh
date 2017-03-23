fe24384ef20f8caca4429e29f3306751468b0388
Christian Pointner <cpointner@mgit.at>
2017-03-23 08:22:23 +0100

    remove legacy scripts and configs

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# https://kubernetes.io/docs/getting-started-guides/gce/
-# https://kubernetes.io/docs/getting-started-guides/network-policy/calico/
-# https://github.com/kubernetes/kubernetes/blob/master/cluster/gce/config-default.sh
-export KUBE_GCE_ZONE=europe-west1-b
-export NETWORK_POLICY_PROVIDER=calico
-export KUBE_NODE_OS_DISTRIBUTION=debian
-export CLUSTER_NAME=netconomy
-curl -sS https://get.k8s.io | bash
